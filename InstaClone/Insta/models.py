e65eb8793896928d8d96039729541e748542e0fd
MarlineKhavele <marlinekhavele6@gmail.com>
2020-01-22 21:39:39 +0300

    Added models

--- InstaClone/Insta/models.py
+++ InstaClone/Insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from PIL import Image
+
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey("auth.user", on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    created_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        self.author
